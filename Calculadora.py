00c2e81635fa7685133668eae553580da0ab04bd
Renan F. Stein <81093045+RenanFStein@users.noreply.github.com>
2022-02-07 15:53:36 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,70 +1,64 @@
-from ast import While
-from itertools import repeat
+def soma (x,y):
+    return (x + y)
 
-print('\nMinha primeira calculadora com Python\n')
 
-# Soma
+def subtracao (x,y):
+    return(x - y)
 
-def soma(x, y):
-    return x + y
+        
+def divisao (x,y):
+    return(x / y)
 
-# Subtração
+        
+def multiplicacao (x,y):
+    return(x * y)
+    
+def sobra (x,y):
+    return (x % y)
 
-def subtracao(x, y):
-    return x - y
+escolha = ()
 
-# Divisão
+while escolha  != '0' or escolha != '1' or escolha != '2' or escolha != '3' or escolha != '4' or escolha != '4' or escolha != '5':
+    print(' \n')   
+    print(' ________Calculadora__________')
+    print('|    Escolha Entre 0 a 5      |')
+    print('|    1 - Somar                |')
+    print('|    2 - Subtrair             |')
+    print('|    3 - Dividir              |')
+    print('|    4 - Multiplicar          |')
+    print('|    5 - Sobra da Divisão     |')
+    print('|    0 - Sair                 |')
+    print('|_____________________________|')
+    escolha = (input('Escolha o Calculo:'))
 
-def divisao(x, y):
-    return x / y
-
-# Multiplicação
-
-def multiplicacao(x, y):
-    return x * y
-
-# Janela de Opções 
-
-print("\nSelecione a operação desejada: \n")
-
-print("1 - Soma")
-print("2 - Subtração")
-print("3 - Multiplicação")
-print("4 - Divisão")
-
-escolha = int(input("\nDigite sua opção (1/2/3/4): "))
-
-while escolha >= 5:
-
-    print('\nEscolha errada!')
-    print('\nEscolha um numero entre 1 e 4\n')
-    print("1 - Soma")
-    print("2 - Subtração")
-    print("3 - Multiplicação")
-    print("4 - Divisão")
-    escolha = int(input("\nDigite sua opção (1/2/3/4): "))
-
-    if escolha == 1:
+    if escolha == '1':
         num1 = int(input("\nDigite o primeiro número: "))
         num2 = int(input("\nDigite o segundo número: "))
         print ('\n A soma de ', num1, '+', num2, 'é =', soma(num1, num2))
 
-    elif escolha == 2:
+    elif escolha == '2':
         num1 = int(input("\nDigite o primeiro número: "))
         num2 = int(input("\nDigite o segundo número: "))
         print ('\n A subtração de ', num1, '-', num2, 'é =', subtracao(num1, num2))
             
-    elif escolha == 3:
+    elif escolha == '3':
         num1 = int(input("\nDigite o primeiro número: "))
         num2 = int(input("\nDigite o segundo número: "))
-        print ('\n A multiplicação de ', num1, '*', num2, 'é =', multiplicacao(num1, num2))
+        print ('\n A multiplicação de ', num1, '*', num2, 'é =', divisao(num1, num2))
         
-    elif escolha == 4:
+    elif escolha == '4':
         num1 = int(input("\nDigite o primeiro número: "))
         num2 = int(input("\nDigite o segundo número: "))
-        print ('\n A divisão de ', num1, '/', num2, 'é =', int(divisao(num1, num2)))
-  
-  
+        print ('\n A divisão de ', num1, '/', num2, 'é =', int(multiplicacao(num1, num2)))
+
+    elif escolha == '5':
+        num1 = int(input("\nDigite o primeiro número: "))
+        num2 = int(input("\nDigite o segundo número: "))
+        print ('\n A sobra da divisão', num1, '/', num2, 'é =', int(sobra(num1, num2)))
+
+    elif escolha == '0':       
+        print ('Obrigado e Volte Sempre' )
+        break
       
 
 
